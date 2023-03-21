a88eb020c168f586a02f219e92635820caa688d0
BorisSoaked <borisrunfast@gmail.com>
2023-03-21 16:16:49 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold text-red-500 text-center underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
