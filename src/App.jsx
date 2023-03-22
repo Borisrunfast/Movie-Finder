96ad7d12ab44074057bab138414ace70b766969b
BorisSoaked <borisrunfast@gmail.com>
2023-03-22 11:50:10 +0100

    installed tailwind and started working on the header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
+import Nav from "./Nav"
 
 function App() {
 
 
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold text-red-500 text-center underline">
-        Hello world!
-      </h1>
+    <div className="App bg-gray-800 w-screen h-screen">
+      <header className="flex align-middle h-10">
+          <h1 className="text-3xl text-green-400 font-bold text-left ml-3 mt-3">
+            MOVIE FINDER
+          </h1>
+          <Nav />
+
+      </header>
+      
     </div>
   )
 }
