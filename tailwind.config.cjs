1c2f5df62546fea86817bf627acc7ed64ad04f8d
BorisSoaked <borisrunfast@gmail.com>
2023-03-26 11:40:00 +0200

    adding nav

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
   content: [
     "./public/index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      
+    },
   },
   plugins: [],
 }
