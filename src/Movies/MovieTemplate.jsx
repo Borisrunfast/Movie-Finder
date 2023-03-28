23c53d2530d431c2143131d598fa8a429f808425
BorisSoaked <borisrunfast@gmail.com>
2023-03-28 20:06:08 +0200

    css for movie template

--- src/Movies/MovieTemplate.jsx
+++ src/Movies/MovieTemplate.jsx
@@ -8,7 +8,7 @@ function MovieTemplate() {
             <img className="w-30 object-cover" src="https://marketplace.canva.com/EAFH3gODxw4/1/0/1131w/canva-black-%26-white-modern-mystery-forest-movie-poster-rLty9dwhGG4.jpg"></img>
             <div className=" h-full w-auto">
                 <h1 className=" ">WOOD</h1>
-                <p className=" text-ellipsis">Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                <p className=" text-ellipsis whitespace-nowrap overflow-hidden">Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
                     Temporibus aut necessitatibus sit non odio, atque beatae quas iure veritatis nisi velit impedit, 
                     aspernatur earum tempore in a molestiae, illo perspiciatis?</p>
                 <button>Checkout</button>
